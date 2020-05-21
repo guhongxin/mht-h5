9e86863feef7810488ba5df9827e11ec7dae2b26
guhongxin <1204738681@qq.com>
2020-05-21 10:56:46 +0800

    基础配置

---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+
+module.exports = {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/style/main.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/vant.js',
+    { src:'@/plugins/amfe-flexible.js', ssr: false }
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxt/typescript-build',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    postcss: [
+      require('postcss-pxtorem')({
+        rootValue: 37.5, // vant-UI的官方根字体大小是37.5
+        propList: ['*']
+      }),
+      require('autoprefixer')({
+        browsers: ['Android >= 4.0', 'iOS >= 7']
+      }),
+    ],
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {}
+})
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/nuxt.scss";
+</style>>
