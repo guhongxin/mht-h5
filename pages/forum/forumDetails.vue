9211bb22ab251e8b8d8a11343f369da64fe9c174
guhongxin <1204738681@qq.com>
2020-07-14 16:29:25 +0800

    12

--- pages/forum/forumDetails.vue
+++ pages/forum/forumDetails.vue
@@ -10,14 +10,10 @@
           <img src="/img/rmgame.png" />
         </div>
         <div class="forum-infor-right">
-<<<<<<< HEAD
           <div class="infor-title">
             <div>末日血战</div>
             <div class="gz">关注</div>
           </div>
-=======
-          <p class="infor-title">末日血战 <span class="gz">关注</span></p>
->>>>>>> 108f7a26bd1dcde7d73d6c858dcbc392fb46cc62
           <p class="tzNum">帖子数：9999</p>
           <p class="scNum">收藏数：9999</p>
           <div class="infor-group">
@@ -34,14 +30,10 @@
     </div>
     <div class="post-area">
       <van-tabs v-model="active" :line-width="20" :line-height="4">
-<<<<<<< HEAD
         <van-tab title="全部">
           <post-item></post-item>
           <post-item></post-item>
         </van-tab>
-=======
-        <van-tab title="全部">内容 1</van-tab>
->>>>>>> 108f7a26bd1dcde7d73d6c858dcbc392fb46cc62
         <van-tab title="精品区">内容 2</van-tab>
         <van-tab title="公告">内容 3</van-tab>
         <van-tab title="攻略">内容 4</van-tab>
@@ -52,7 +44,6 @@
   </div>
 </template>
 <script lang="ts">
-<<<<<<< HEAD
 import { Vue, Component } from "vue-property-decorator";
 import PostItem from "~/components/PostItem.vue";
 @Component({
@@ -60,10 +51,6 @@ import PostItem from "~/components/PostItem.vue";
     PostItem
   }
 })
-=======
-import { Vue, Component } from "vue-property-decorator"
-@Component
->>>>>>> 108f7a26bd1dcde7d73d6c858dcbc392fb46cc62
 export default class ForumDetails extends Vue {
   private active:number = 0;
 }
@@ -72,14 +59,9 @@ export default class ForumDetails extends Vue {
 .forum-details {
   margin-top: -56px;
   .head {
-<<<<<<< HEAD
     height: 180px;
     padding: 0px 10px;
     position: relative;
-=======
-    height: 250px;
-    padding: 0px 10px;
->>>>>>> 108f7a26bd1dcde7d73d6c858dcbc392fb46cc62
     .title {
       height: 50px;
       color: #ffffff;
@@ -88,10 +70,7 @@ export default class ForumDetails extends Vue {
       text-align: center;
       line-height: 50px;
       position: relative;
-<<<<<<< HEAD
       z-index: 1;
-=======
->>>>>>> 108f7a26bd1dcde7d73d6c858dcbc392fb46cc62
       .back-icon {
         position: absolute;
         top: calc(25px - 8px);
@@ -99,7 +78,6 @@ export default class ForumDetails extends Vue {
       }
     }
   }
-<<<<<<< HEAD
   .head::after {
     width: 100%;
     content: "";
@@ -109,25 +87,16 @@ export default class ForumDetails extends Vue {
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
   }
-=======
->>>>>>> 108f7a26bd1dcde7d73d6c858dcbc392fb46cc62
   .forum-infor {
     display: flex;
     justify-content: start;
     align-items: center;
-<<<<<<< HEAD
     z-index: 1;
     position: relative;
     .forum-infor-left {
       img {
         width: 90px;
         height: 90px;
-=======
-    .forum-infor-left {
-      img {
-        width: 64px;
-        height: 64px;
->>>>>>> 108f7a26bd1dcde7d73d6c858dcbc392fb46cc62
         display: block;
       }
     }
@@ -138,7 +107,6 @@ export default class ForumDetails extends Vue {
         font-size: 16px;
         color: #ffffff;
         font-weight: 600;
-<<<<<<< HEAD
         margin-bottom: 2px;
         display: flex;
         justify-content: space-between;
@@ -150,45 +118,23 @@ export default class ForumDetails extends Vue {
           color: rgba(125, 179, 73, 1);
           border:1px solid rgba(125, 179, 73, 1);
           border-radius: 4px;
-=======
-        margin-bottom: 5px;
-        .gz {
-          float: right;
-          display: inline-block;
-          padding: 4px 5px;
-          font-size: 14px;
-          line-height: normal;
->>>>>>> 108f7a26bd1dcde7d73d6c858dcbc392fb46cc62
         }
       }
       .tzNum, .scNum {
         font-size: 12px;
         color: #ffffff;
-<<<<<<< HEAD
         margin-bottom: 2px;
-=======
->>>>>>> 108f7a26bd1dcde7d73d6c858dcbc392fb46cc62
       }
       .infor-group {
         display: flex;
         justify-content: start;
         align-items: center;
-<<<<<<< HEAD
         margin-top: 10px;
         div {
           font-size: 14px;
           color: #7DB349;
           font-weight: bold;
           flex: 1;
-=======
-        div {
-          font-size: 12px;
-          color: #ffffff;
-          padding-bottom: 5px;
-        }
-        &> div:not(:last-child) {
-          padding-right: 4px;
->>>>>>> 108f7a26bd1dcde7d73d6c858dcbc392fb46cc62
         }
       }
     }
